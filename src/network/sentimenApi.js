6b0c07cd854522d35a226e1d0bc2a03e4020e192
Megianto Adi Saputra <72857078+MegiantoAS@users.noreply.github.com>
2023-09-11 15:08:48 +0700

    update2

--- src/network/sentimenApi.js
+++ src/network/sentimenApi.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyBVbD1o9xk1IdSiXF14xxRRtmYgmNcdSAQ' // Ganti dengan API key sentimen Anda
+const API_KEY = process.env.REACT_APP_KEY_GOOGLE;
 
 const sentimentApi = axios.create({
-  baseURL: 'https://language.googleapis.com/v1', // URL API sentiment harus dipisahkan dari path '/documents:analyzeSentiment'
+  baseURL: 'https://language.googleapis.com/v1', 
 });
 
 export const analyzeSentiment = async (text) => {
   try {
     const response = await sentimentApi.post(
-      `/documents:analyzeSentiment?key=${API_KEY}`, // Ubah URL dengan path '/documents:analyzeSentiment' dan tambahkan query parameter 'key' di sini
+      `/documents:analyzeSentiment?key=${API_KEY}`, 
       {
         document: {
           content: text,
--- src/pages/NewsCards.js
+++ src/pages/NewsCards.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import { Grid, Card, CardContent, CardMedia, Box, Typography, SvgIcon } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Box,
+  Typography,
+  SvgIcon,
+} from '@mui/material';
 import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
 import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 
+import { detectAndTranslateText } from '../network/languageAPI'; // Sesuaikan dengan path file
 
 function NewsCard({ article }) {
+  const [translatedTitle, setTranslatedTitle] = useState('');
+  const [translatedDescription, setTranslatedDescription] = useState('');
+
+  useEffect(() => {
+    const translateTitleAndDescription = async () => {
+      const translatedTitleText = await detectAndTranslateText(article.title, 'id');
+      setTranslatedTitle(translatedTitleText);
+
+      const translatedDescriptionText = await detectAndTranslateText(article.description, 'id');
+      setTranslatedDescription(translatedDescriptionText);
+    };
+
+    translateTitleAndDescription();
+  }, [article.title, article.description]);
+
   return (
     <Grid item xs={12} md={4} key={article.url}>
-      <a href={article.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
-        <Card style={{ cursor: 'pointer', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)', borderRadius: '8px' }}>
+      <a
+        href={article.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: 'none', color: 'inherit' }}
+      >
+        <Card
+          style={{
+            cursor: 'pointer',
+            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+            borderRadius: '8px',
+          }}
+        >
           <CardMedia
             component="img"
             height="200"
@@ -18,9 +53,9 @@ function NewsCard({ article }) {
             style={{ objectFit: 'cover' }}
           />
           <CardContent>
-            <Typography variant="h6">{article.title}</Typography>
+            <Typography variant="h6">{translatedTitle}</Typography>
             <Typography variant="body2">
-              {article.description}
+              {translatedDescription}
             </Typography>
             {article.sentiment && (
               <Box
@@ -42,19 +77,28 @@ function NewsCard({ article }) {
               >
                 {article.sentiment === 'Positif' && (
                   <>
-                    <SvgIcon component={SentimentSatisfiedIcon} style={{ marginRight: '8px' }} />
+                    <SvgIcon
+                      component={SentimentSatisfiedIcon}
+                      style={{ marginRight: '8px' }}
+                    />
                     Konten ini memuat berita {article.sentiment}. Selamat membaca.
                   </>
                 )}
                 {article.sentiment === 'Negatif' && (
                   <>
-                    <SvgIcon component={SentimentVeryDissatisfiedIcon} style={{ marginRight: '8px' }} />
-                    Konten ini memuat berita {article.sentiment}. Mengandung unsur sara, budaya, agama yang sensitif. Pastikan bijak dalam membaca.
+                    <SvgIcon
+                      component={SentimentVeryDissatisfiedIcon}
+                      style={{ marginRight: '8px' }}
+                    />
+                    Konten ini memuat berita {article.sentiment}. Mengandung unsur kata kekerasan, sara, budaya, agama yang sensitif. Pastikan bijak dalam membaca.
                   </>
                 )}
                 {article.sentiment === 'Netral' && (
                   <>
-                    <SvgIcon component={SentimentDissatisfiedIcon} style={{ marginRight: '8px' }} />
+                    <SvgIcon
+                      component={SentimentDissatisfiedIcon}
+                      style={{ marginRight: '8px' }}
+                    />
                     Konten ini memuat berita {article.sentiment}. Harap bijak dalam membaca.
                   </>
                 )}
